f203bcedaaaf45f443a82c904c8b8601f990a59e
adriana <adriana9527@gmail.com>
2017-11-30 14:08:44 -0500

    Add session class

--- src/session.ts
+++ src/session.ts
@@ -1,4 +1,15 @@
-export { Session };
-class Session {
+import {Entity} from './entity';
 
+export { Session };
+class Session extends Entity {
+private userId : string
+public setUserId(userId: string) : string
+{
+  this.userId = userId;
+  return userId;
+}
+public getUserId()
+{
+  return this.userId;
+}
 }
