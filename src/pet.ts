fca1782dc30a5d0cf3b7fd6ae729d4131b2055a9
adriana <adriana9527@gmail.com>
2017-11-30 13:08:46 -0500

    Add Pet class

--- src/pet.ts
+++ src/pet.ts
@@ -1,4 +1,50 @@
+import {Entity} from './entity';
+import * as moment from 'moment';
 export { Pet };
-class Pet {
+class Pet extends Entity {
+private name: string;
+private birthDate: number;
+private breed: number;
+private species: number;
 
+public getName() : string {
+return this.name;
+}
+public getBirthDate(type: number = 0) : number | string {
+  switch (type){
+    // number
+    case 0:
+      return this.birthDate;
+    // string1
+    case 1:
+      return moment(this.birthDate).format("dddd, MMMM Do YYYY");
+    default:
+      return this.birthDate;
+  }
+}
+public getBreed() : number {
+  return this.breed;
+}
+public getSpecies() : number {
+  return this.species;
+}
+public setName(name: string) : string
+{
+  this.name = name;
+  return this.name;
+}
+public setBreed(breed: number) : number {
+  this.breed = breed;
+  return this.breed;
+}
+//0 for dog 1 for cat
+public setSpecies(species: number) : number {
+  this.species = species;
+  return this.species;
+}
+public setBirthDate(birthDate:number) : number
+{
+  this.birthDate = birthDate
+  return this.birthDate;
+}
 }
