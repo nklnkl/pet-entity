062d91e5d50f5ceb284de0891760d992178e6373
Niko Lagman <Lagman.Niko@gmail.com>
2017-12-02 04:42:29 -0500

    Refactor Entity class for json usage

--- src/entity.ts
+++ src/entity.ts
@@ -5,24 +5,40 @@ class Entity {
   private created: number;
   private updated: number;
 
+  constructor (json?: string) {
+    if (json) {
+      let object: any = JSON.parse(json);
+      this.setId(object.id);
+      this.setCreated(object.created);
+      this.setUpdated(object.updated);
+    }
+    else {
+      this.setId('null');
+      this.setCreated(0);
+      this.setUpdated(0);
+    }
+  }
+
   public getId () : string {
     return this.id;
   }
 
   //type 0: returns numeric (default), 1  returns string
   public getCreated (type?: number) : number | string {
-    if (type == 0 || type == undefined)
-      return this.created;
-    else
-      return Moment(this.created).format("dddd, MMMM Do YYYY");
+    switch(type) {
+      case 0: return this.created;
+      case 1: return Moment(this.created).format("dddd, MMMM Do YYYY");
+      default: return this.created;
+    }
   }
 
   //type 0: returns numeric (default), 1  returns string
   public getUpdated (type?: number) : number | string {
-    if (type == 0 || type == undefined)
-      return this.updated;
-    else
-      return Moment(this.updated).format("dddd, MMMM Do YYYY");
+    switch(type) {
+      case 0: return this.updated;
+      case 1: return Moment(this.updated).format("dddd, MMMM Do YYYY");
+      default: return this.updated;
+    }
   }
 
   public setId (id: string) : string {
