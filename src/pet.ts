0535b5e7b10cb7d897f1ebe54438fbee9cc2ddfd
Niko Lagman <Lagman.Niko@gmail.com>
2017-12-02 04:42:54 -0500

    Refactor Pet class for json usage

--- src/pet.ts
+++ src/pet.ts
@@ -1,64 +1,78 @@
-import {Entity} from './entity';
-import * as moment from 'moment';
+import { Entity } from './entity';
+import { Breeds, Species } from './species';
+import * as Moment from 'moment';
 export { Pet };
+
 class Pet extends Entity {
-private name: string;
-private birthDate: number;
-private breed: number;
-private species: number;
+  private name: string;
+  private birthDate: number;
+  private breed: number;
+  private species: number;
 
-public getName() : string {
-return this.name;
-}
-public getBirthDate(type: number = 0) : number | string {
-  switch (type){
-    // number
-    case 0:
-      return this.birthDate;
-    // string1
-    case 1:
-      return moment(this.birthDate).format("dddd, MMMM Do YYYY");
-    default:
-      return this.birthDate;
-  }
-}
-public getBreed() : number {
-  return this.breed;
-}
-public getSpecies() : number {
-  return this.species;
-}
-public getAge(type:number=0) : number {
-  switch (type)
-  {
-    case 0:
-      return moment().diff( moment(this.birthDate), "weeks");
-    case 1:
-      return moment().diff( moment(this.birthDate), "months");
-    case 2:
-      return moment().diff( moment(this.birthDate), "years");
-    default:
-    return -1; //error
+  constructor (json?: string) {
+    if (json) {
+      super(json);
+      let object: any = JSON.parse(json);
+      this.setName(object.name);
+      this.setBirthDate(object.birthDate);
+      this.setBreed(object.breed);
+      this.setSpecies(object.species);
+    }
+    else {
+      super();
+      this.setName('null');
+      this.setBirthDate(0);
+      this.setBreed(0);
+      this.setSpecies(0);
+    }
   }
 
-}
-public setName(name: string) : string
-{
-  this.name = name;
-  return this.name;
-}
-public setBreed(breed: number) : number {
-  this.breed = breed;
-  return this.breed;
-}
-//0 for dog 1 for cat
-public setSpecies(species: number) : number {
-  this.species = species;
-  return this.species;
-}
-public setBirthDate(birthDate:number) : number
-{
-  this.birthDate = birthDate
-  return this.birthDate;
-}
+  public getName() : string {
+    return this.name;
+  }
+  public getBirthDate(type: number = 0) : number|string {
+    switch (type){
+      case 0: return this.birthDate;
+      case 1: return Moment(this.birthDate).format("dddd, MMMM Do YYYY");
+      default: return this.birthDate;
+    }
+  }
+  public getBreed(type: number = 0) : number|string {
+    switch(type) {
+      case 0: return this.breed;
+      case 1: return Breeds.getList()[this.species][this.breed];
+      default: return this.breed;
+    }
+  }
+  public getSpecies(type: number = 0) : number|string {
+    switch(type) {
+      case 0: return this.species;
+      case 1: return Species.getList()[this.species];
+      default: return this.species;
+    }
+  }
+  public getAge(unit: number = 0) : number {
+    switch (unit) {
+      case 0: return Moment().diff( Moment(this.birthDate), "weeks");
+      case 1: return Moment().diff( Moment(this.birthDate), "months");
+      case 2: return Moment().diff( Moment(this.birthDate), "years");
+      default: return Moment().diff( Moment(this.birthDate), "weeks");
+    }
+  }
+  public setName(name: string) : string {
+    this.name = name;
+    return this.name;
+  }
+  public setBreed(breed: number) : number {
+    this.breed = breed;
+    return this.breed;
+  }
+  public setSpecies(species: number) : number {
+    this.species = species;
+    return this.species;
+  }
+  public setBirthDate(birthDate:number) : number {
+    this.birthDate = birthDate
+    return this.birthDate;
+  }
 }
