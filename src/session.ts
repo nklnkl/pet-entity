5cd5c7d9030009f17bc52eb160381e74e9dea885
Niko Lagman <Lagman.Niko@gmail.com>
2017-12-02 04:43:07 -0500

    Refactor Session class for json usage

--- src/session.ts
+++ src/session.ts
@@ -1,15 +1,26 @@
 import {Entity} from './entity';
-
 export { Session };
+
 class Session extends Entity {
-private userId : string
-public setUserId(userId: string) : string
-{
-  this.userId = userId;
-  return userId;
-}
-public getUserId()
-{
-  return this.userId;
-}
+  private userId : string;
+
+  constructor (json?: string) {
+    if (json) {
+      super(json);
+      let object: any = JSON.parse(json);
+      this.setUserId(object.userId);
+    }
+    else {
+      super();
+      this.setUserId('null');
+    }
+  }
+
+  public setUserId(userId: string) : string {
+    this.userId = userId;
+    return userId;
+  }
+  public getUserId() {
+    return this.userId;
+  }
 }
