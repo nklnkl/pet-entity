045d7e7a2c661e71d98ad5b982e9445da116494e
Niko Lagman <Lagman.Niko@gmail.com>
2017-12-04 07:44:45 -0500

    Update index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import { Entity } from './object/entity';
 import { Account } from './object/account';
 import { Pet } from './object/pet';
 import { Session } from './object/session';
 import { Breeds, Species } from './object/species';
 export {
+  Entity,
   Account,
   Pet,
   Session,
